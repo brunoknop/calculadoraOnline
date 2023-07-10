3be0901a9a667a8dc3ea83b4b50b061bc1b24123
brunoknop <brunog.knop@gmail.com>
2023-07-10 13:51:49 -0300

    adição dos hovers dos botões e inicio do script

---
+++ script.js
@@ -0,0 +1,38 @@
+//variaveis globais para transporte de dados
+let _entradaAtual
+let lblVisor
+
+//elementos da calculadora
+const lblresultadoVisor = document.getElementById(lblresultadoVisor)
+const resultadoVisor = document.getElementById(resultadoVisor)
+const limpeza = document.getElementById(limpeza)
+const maisMenos = document.getElementById(maisMenos)
+const cento = document.getElementById(cento)
+const divisao = document.getElementById(divisao)
+const multiplicacao = document.getElementById(multiplicacao)
+const subtracao = document.getElementById(subtracao)
+const adicao = document.getElementById(adicao)
+const resultado = document.getElementById(resultado)
+const virg = document.getElementById(virg)
+const num0 = document.getElementById(num0)
+const num1 = document.getElementById(num1)
+const num2 = document.getElementById(num2)
+const num3 = document.getElementById(num3)
+const num4 = document.getElementById(num4)
+const num5 = document.getElementById(num5)
+const num6 = document.getElementById(num6)
+const num7 = document.getElementById(num7)
+const num8 = document.getElementById(num8)
+const num9 = document.getElementById(num9)
+
+function MostrarResultado(resultado) {
+  //apresenta o resultado no visor
+}
+
+function AlteraLblVisor(operacao) {
+  //apresenta a conta respectiva ao resultado
+}
+
+function LimparVisor() {
+  //limpa tanto o visor do resultado, quanto o lbl
+}
