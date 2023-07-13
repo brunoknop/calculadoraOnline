80fef7180bfbd5a0e35bebd77928f6f114b18833
brunoknop <brunog.knop@gmail.com>
2023-07-12 22:54:13 -0300

    inicio do script dos botões

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-//variaveis globais para transporte de dados
-let _entradaAtual
-let lblVisor
+//Variaveis globais para transporte de dados
+let _conteudoLblVisor = []
+let _conteudoVisor = []
 
-//elementos da calculadora
-const lblresultadoVisor = document.getElementById(lblresultadoVisor)
-const resultadoVisor = document.getElementById(resultadoVisor)
-const limpeza = document.getElementById(limpeza)
-const maisMenos = document.getElementById(maisMenos)
-const cento = document.getElementById(cento)
-const divisao = document.getElementById(divisao)
-const multiplicacao = document.getElementById(multiplicacao)
-const subtracao = document.getElementById(subtracao)
-const adicao = document.getElementById(adicao)
-const resultado = document.getElementById(resultado)
-const virg = document.getElementById(virg)
-const num0 = document.getElementById(num0)
-const num1 = document.getElementById(num1)
-const num2 = document.getElementById(num2)
-const num3 = document.getElementById(num3)
-const num4 = document.getElementById(num4)
-const num5 = document.getElementById(num5)
-const num6 = document.getElementById(num6)
-const num7 = document.getElementById(num7)
-const num8 = document.getElementById(num8)
-const num9 = document.getElementById(num9)
+//Visor da calculadora
+const lblresultadoVisor = document.getElementById("lblresultadoVisor")
+const resultadoVisor = document.getElementById("resultadoVisor")
 
+//Botões auxiliares e suas funcionalidades
+const limpeza = document.getElementById("limpeza")
+limpeza.addEventListener("click", LimparVisor)
+
+const maisMenos = document.getElementById("maisMenos")
+
+const cento = document.getElementById("cento")
+
+
+//Botões operadores e suas funcionalidades
+const divisao = document.getElementById("divisao")
+
+const multiplicacao = document.getElementById("multiplicacao")
+
+const subtracao = document.getElementById("subtracao")
+
+const adicao = document.getElementById("adicao")
+
+const resultado = document.getElementById("resultado")
+
+//Botões de números e suas funcionalidades
+let botoes = document.getElementsByClassName("numeros")
+
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].addEventListener("click", function () {
+    AdicionaNumeroVisor(this.textContent)
+    limpeza.textContent = "C"
+  })
+}
+
+//funções da calculadora
 function MostrarResultado(resultado) {
   //apresenta o resultado no visor
 }
@@ -34,5 +46,13 @@ function AlteraLblVisor(operacao) {
 }
 
 function LimparVisor() {
-  //limpa tanto o visor do resultado, quanto o lbl
+  _conteudoLblVisor = []
+  _conteudoVisor = []
+  lblresultadoVisor.textContent = ""
+  resultadoVisor.value = ""
+  limpeza.textContent = "AC"
+}
+
+function AdicionaNumeroVisor(numero) {
+  resultadoVisor.value += numero
 }
