b28a6995142e8f4b06693e1051287bfb2fbd7f68
bruno.knop <bruno.knop@cwi.com.br>
2023-07-14 21:08:47 -0300

    continuação da adição das funcionalidades aos botões

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Variaveis globais para transporte de dados
 let _conteudoLblVisor = []
-let _conteudoVisor = []
 
 //Visor da calculadora
 const lblresultadoVisor = document.getElementById("lblresultadoVisor")
@@ -8,51 +7,83 @@ const resultadoVisor = document.getElementById("resultadoVisor")
 
 //Botões auxiliares e suas funcionalidades
 const limpeza = document.getElementById("limpeza")
-limpeza.addEventListener("click", LimparVisor)
+limpeza.addEventListener("click", function () {
+  _conteudoLblVisor = []
+  LimparVisor()
+  limpeza.textContent = "AC"
+})
 
 const maisMenos = document.getElementById("maisMenos")
-
 const cento = document.getElementById("cento")
 
-
 //Botões operadores e suas funcionalidades
-const divisao = document.getElementById("divisao")
+let operadores = document.getElementsByClassName("operadores")
 
-const multiplicacao = document.getElementById("multiplicacao")
-
-const subtracao = document.getElementById("subtracao")
-
-const adicao = document.getElementById("adicao")
-
-const resultado = document.getElementById("resultado")
+for (let n = 0; n < operadores.length; n++) {
+  operadores[n].addEventListener("click", function () {
+    if (this.textContent == "=") ApresentarResultado()
+    else {
+      if (_conteudoLblVisor.length == 3) _conteudoLblVisor = []
+      if (_conteudoLblVisor.length <= 0) {
+        AdicionarValorAoLbl(resultadoVisor.value)
+        AdicionarValorAoLbl(this.textContent)
+      } else {
+        _conteudoLblVisor[1] = this.textContent
+      }
+      AtualizarLblVisor()
+    }
+  })
+}
 
 //Botões de números e suas funcionalidades
 let botoes = document.getElementsByClassName("numeros")
 
-for (let i = 0; i < botoes.length; i++) {
-  botoes[i].addEventListener("click", function () {
+for (let n = 0; n < botoes.length; n++) {
+  botoes[n].addEventListener("click", function () {
+    if (_conteudoLblVisor.length == 2) resultadoVisor.value = "" //resolver questão da limpeza da tela
     AdicionaNumeroVisor(this.textContent)
     limpeza.textContent = "C"
   })
 }
 
-//funções da calculadora
-function MostrarResultado(resultado) {
-  //apresenta o resultado no visor
-}
-
-function AlteraLblVisor(operacao) {
-  //apresenta a conta respectiva ao resultado
+function AtualizarLblVisor() {
+  lblresultadoVisor.textContent = ""
+  for (let n = 0; n < _conteudoLblVisor.length; n++) {
+    lblresultadoVisor.textContent += _conteudoLblVisor[n]
+  }
 }
 
 function LimparVisor() {
-  _conteudoLblVisor = []
-  _conteudoVisor = []
   lblresultadoVisor.textContent = ""
   resultadoVisor.value = ""
-  limpeza.textContent = "AC"
 }
 
 function AdicionaNumeroVisor(numero) {
   resultadoVisor.value += numero
+}
+
+function AdicionarValorAoLbl(valor) {
+  _conteudoLblVisor.push(valor)
+}
+
+function ApresentarResultado() {
+  let resultado
+  switch (_conteudoLblVisor[1]) {
+    case "+":
+      //executa a soma
+      break
+
+    case "-":
+      // executa subtração
+      break
+
+    case "÷":
+      //exevuta a divisão
+      break
+
+    case "×":
+      //executa a multiplicação
+      break
+  }
+  resultadoVisor.value = resultado
 }
